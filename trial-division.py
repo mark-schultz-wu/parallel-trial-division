89f0ec8c25bafeaa15a57a7b3c866cab718551e4
Mark Schultz-Wu <mark@schultz-wu.com>
2024-11-20 18:17:40 -0800

    Remove (broken) parallel python impl

--- trial-division.py
+++ trial-division.py
@@ -4,38 +4,18 @@
 import math
 from tqdm import tqdm
 
-
-
-import concurrent.futures
-
 def trial_divide(N, i):
     if N % i == 0:
         return (i, True)
     return (0, False)
 
-def parallel_trial_division(N):
-    factors = []
-    while N % 2 != 0:
-        factors.append(2)
-        N //= 2
-    sqrt_N = math.ceil(math.sqrt(N))
-    odd_numbers = range(3, sqrt_N, 2)
-    with tqdm(odd_numbers) as possible_divisors:
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = executor.map(lambda i: trial_divide(N,i), odd_numbers)
-            for possible_factor in concurrent.futures.as_completed(futures):
-                possible_divisors.update()
-                if possible_factor[1]:
-                    factors.append(possible_factor[0])
-    return factors
-
-def serial_trial_division(N):
+def trial_division(N):
     sqrt_N = math.ceil(math.sqrt(N))
     factors = []
     while N % 2 != 0:
         factors.append(2)
         N //= 2
-    for i in tqdm.trange(3, sqrt_N, 2):
+    for i in tqdm(range(3, sqrt_N, 2)):
         if N % i == 0:
             factors.append(i)
             N //= i
@@ -44,11 +24,6 @@ def serial_trial_division(N):
 
 if __name__ == "__main__":
     N = 237540380304900134239
-    # serial_trial_division(N)
-    parallel_trial_division(N)
-    p = 2**13 - 1
-    q = 2**17 - 1
-    Nsmall = p*q
-    # parallel_trial_division(Nsmall)
+    trial_division(N)
 
 
