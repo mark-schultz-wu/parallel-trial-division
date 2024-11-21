42a5cf8e426ef3944edabbcd5c6e596a14fc4df8
Mark Schultz-Wu <mark@schultz-wu.com>
2024-11-20 20:45:33 -0800

    Fix bug in python code

--- trial-division.py
+++ trial-division.py
@@ -4,15 +4,10 @@
 import math
 from tqdm import tqdm
 
-def trial_divide(N, i):
-    if N % i == 0:
-        return (i, True)
-    return (0, False)
-
 def trial_division(N):
     sqrt_N = math.ceil(math.sqrt(N))
     factors = []
-    while N % 2 != 0:
+    while N % 2 == 0:
         factors.append(2)
         N //= 2
     for i in tqdm(range(3, sqrt_N, 2)):
@@ -21,9 +16,14 @@ def trial_division(N):
             N //= i
         if N == 1:
             return factors
+    factors.append(N)
+    return factors
 
 if __name__ == "__main__":
+    M13 = (1<<13)-1
+    M17 = (1<<17)-1
+    N0 = M13 * M17
     N = 237540380304900134239
-    trial_division(N)
+    print(trial_division(N))
 
 
